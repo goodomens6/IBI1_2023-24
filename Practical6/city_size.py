1a5eeaba4adca7df868e13e843e9d8088ff02658
Jingyan <jingyan.23@intl.zju.edu.cn>
2024-03-20 16:08:54 +0800

    add city_size.py

--- Practical6/city_size.py
+++ Practical6/city_size.py
@@ -1,8 +1,8 @@
-uk_cities=[0.56,0.62,0.04,9.7]
+uk_cities=[0.56,0.62,0.04,9.7]#you can enter numbers that you want to be sorted in order
 china_cities=[0.58,8.4,29.9,22.2]
-uk_cities.sort()
+uk_cities.sort()#make values be sorted in order
 china_cities.sort()
-print(uk_cities)
+print(uk_cities)#print sorted values
 print(china_cities)
 uk_city_names=['Edinburgh','Glasgow','Stirling','London']
 china_city_names=['Haining','hanghou','Shanghai','Beijing']
@@ -10,14 +10,14 @@ china_city_names=['Haining','hanghou','Shanghai','Beijing']
 import numpy as np
 import matplotlib.pyplot as plt
 plt.figure(1)
-plt.bar(uk_city_names,uk_cities)
+plt.bar(uk_city_names,uk_cities)#make bar plot of city sizes in the UK
 plt.ylabel('Population(millions)')
-plt.title('Population of cities in the UK')
+plt.title('City sizes in the UK')
 
 plt.figure(2)
-plt.bar(china_city_names,china_cities)
+plt.bar(china_city_names,china_cities)#make bar plot of city sizes in China
 plt.ylabel('Population(millions)')
-plt.title('Population of cities in China')
+plt.title('City sizes in China')
 
-plt.show()
+plt.show()#show the two bar plots
 plt.clf()
